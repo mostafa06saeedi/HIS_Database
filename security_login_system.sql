8880f22d638662a0d2abffe92f9305d15420b080
AMiR-Mofateh <amirmofateh11@gmail.com>
2026-07-06 16:10:49 +0330

    second part of security_login_system

--- security_login_system.sql
+++ security_login_system.sql
@@ -77,3 +77,90 @@ BEGIN
   VALUES (@username, HASHBYTES('SHA2_256', CONVERT(nvarchar(36), @salt) + @password), @salt, @role, @employeeID)
 END
 GO
+
+CREATE PROCEDURE [sp_Login]
+  @username nvarchar(100),
+  @password nvarchar(100),
+  @success  bit OUTPUT,
+  @message  nvarchar(255) OUTPUT
+AS
+BEGIN
+  SET NOCOUNT ON
+
+  DECLARE @id int, @hash varbinary(64), @salt uniqueidentifier,
+          @role nvarchar(50), @patientID nvarchar(255), @employeeID int, @isActive bit
+
+  SELECT @id = [id], @hash = [passwordHash], @salt = [passwordSalt], @role = [role],
+         @patientID = [patientID], @employeeID = [employeeID], @isActive = [isActive]
+  FROM [UserAccount]
+  WHERE [username] = @username
+
+  IF @id IS NULL
+  BEGIN
+    SET @success = 0
+    SET @message = N'Username not found.'
+    RETURN
+  END
+
+  IF @isActive = 0
+  BEGIN
+    SET @success = 0
+    SET @message = N'This account has been deactivated.'
+    RETURN
+  END
+
+  IF HASHBYTES('SHA2_256', CONVERT(nvarchar(36), @salt) + @password) <> @hash
+  BEGIN
+    SET @success = 0
+    SET @message = N'Incorrect password.'
+    RETURN
+  END
+
+  EXEC [sys].[sp_set_session_context] @key = N'UserID',     @value = @id
+  EXEC [sys].[sp_set_session_context] @key = N'Role',       @value = @role
+  EXEC [sys].[sp_set_session_context] @key = N'PatientID',  @value = @patientID
+  EXEC [sys].[sp_set_session_context] @key = N'EmployeeID', @value = @employeeID
+
+  UPDATE [UserAccount] SET [lastLoginAt] = GETDATE() WHERE [id] = @id
+
+  SET @success = 1
+  SET @message = N'Login successful - Role: ' + @role
+END
+GO
+
+CREATE PROCEDURE [sp_Logout]
+AS
+BEGIN
+  SET NOCOUNT ON
+
+  EXEC [sys].[sp_set_session_context] @key = N'UserID',     @value = NULL
+  EXEC [sys].[sp_set_session_context] @key = N'Role',       @value = NULL
+  EXEC [sys].[sp_set_session_context] @key = N'PatientID',  @value = NULL
+  EXEC [sys].[sp_set_session_context] @key = N'EmployeeID', @value = NULL
+END
+GO
+
+CREATE PROCEDURE [sp_ChangePassword]
+  @username     nvarchar(100),
+  @oldPassword  nvarchar(100),
+  @newPassword  nvarchar(100)
+AS
+BEGIN
+  SET NOCOUNT ON
+
+  DECLARE @hash varbinary(64), @salt uniqueidentifier
+  SELECT @hash = [passwordHash], @salt = [passwordSalt] FROM [UserAccount] WHERE [username] = @username
+
+  IF @hash IS NULL OR HASHBYTES('SHA2_256', CONVERT(nvarchar(36), @salt) + @oldPassword) <> @hash
+  BEGIN
+    RAISERROR(N'Incorrect username or current password.', 16, 1)
+    RETURN
+  END
+
+  DECLARE @newSalt uniqueidentifier = NEWID()
+  UPDATE [UserAccount]
+  SET [passwordHash] = HASHBYTES('SHA2_256', CONVERT(nvarchar(36), @newSalt) + @newPassword),
+      [passwordSalt] = @newSalt
+  WHERE [username] = @username
+END
+GO
