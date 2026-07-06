50667a118963d0e1b1884cb775f27d3b35d1cdd2
AMiR-Mofateh <amirmofateh11@gmail.com>
2026-07-06 11:32:55 +0330

    second part of Trigger

--- triggers.sql
+++ triggers.sql
@@ -98,3 +98,96 @@ BEGIN
         )
 END
 GO
+
+CREATE TRIGGER [trg_iotlog_alert] ON [logs]
+AFTER INSERT
+AS
+BEGIN
+  SET NOCOUNT ON
+
+  ;WITH [DeviceContext] AS (
+    SELECT
+      [i].[id]        AS [logID],
+      [i].[deviceID],
+      [i].[type],
+      [i].[value],
+      [dt].[patientID]
+    FROM [inserted] AS [i]
+    OUTER APPLY (
+      SELECT TOP 1 [dt2].[patientID]
+      FROM [devicetransfer] AS [dt2]
+      WHERE [dt2].[iotdeviceID] = [i].[deviceID] AND [dt2].[unassignedAt] IS NULL
+      ORDER BY [dt2].[assignedAt] DESC
+    ) AS [dt]
+  ),
+  [MatchedThreshold] AS (
+    SELECT
+      [dc].[logID],
+      [dc].[value],
+      [at].[id]       AS [thresholdID],
+      [at].[minValue],
+      [at].[maxValue],
+      [at].[severity],
+      ROW_NUMBER() OVER (
+        PARTITION BY [dc].[logID]
+        ORDER BY CASE WHEN [dc].[patientID] IS NOT NULL AND [at].[patientID] = [dc].[patientID] THEN 0
+                      ELSE 1 END
+      ) AS [rn]
+    FROM [DeviceContext] AS [dc]
+    INNER JOIN [AlertThreshold] AS [at]
+      ON [at].[measurementType] = [dc].[type]
+     AND ( ([dc].[patientID] IS NOT NULL AND [at].[patientID] = [dc].[patientID]) OR [at].[isGlobal] = 1 )
+  )
+  INSERT INTO [alert] ([logID], [alertThresholdID], [severity], [status], [createdtime])
+  SELECT [mt].[logID], [mt].[thresholdID], [mt].[severity], N'Unreviewed', GETDATE()
+  FROM [MatchedThreshold] AS [mt]
+  WHERE [mt].[rn] = 1
+    AND (
+          ([mt].[minValue] IS NOT NULL AND [mt].[value] < [mt].[minValue])
+       OR ([mt].[maxValue] IS NOT NULL AND [mt].[value] > [mt].[maxValue])
+        )
+END
+GO
+
+CREATE TRIGGER [trg_devicetransfer_close_previous] ON [devicetransfer]
+AFTER INSERT
+AS
+BEGIN
+  SET NOCOUNT ON
+
+  UPDATE [dt]
+  SET [dt].[unassignedAt] = [i].[assignedAt]
+  FROM [devicetransfer] AS [dt]
+  INNER JOIN [inserted] AS [i] ON [i].[iotdeviceID] = [dt].[iotdeviceID]
+  WHERE [dt].[unassignedAt] IS NULL AND [dt].[id] <> [i].[id]
+END
+GO
+
+CREATE TRIGGER [trg_storagetransaction_update_inventory] ON [storage_transaction]
+AFTER INSERT
+AS
+BEGIN
+  SET NOCOUNT ON
+
+  UPDATE [s] SET [s].[inventory] = [s].[inventory] + [i].[quantity]
+  FROM [storage] AS [s]
+  INNER JOIN [inserted] AS [i] ON [i].[storageID] = [s].[id]
+  WHERE [i].[type] = N'IN'
+
+  UPDATE [s] SET [s].[inventory] = [s].[inventory] - [i].[quantity]
+  FROM [storage] AS [s]
+  INNER JOIN [inserted] AS [i] ON [i].[storageID] = [s].[id]
+  WHERE [i].[type] = N'OUT'
+
+  IF EXISTS (
+    SELECT 1
+    FROM [storage] AS [s]
+    WHERE [s].[inventory] < 0
+      AND [s].[id] IN (SELECT DISTINCT [storageID] FROM [inserted])
+  )
+  BEGIN
+    RAISERROR(N'Insufficient inventory for this transaction.', 16, 1)
+    RETURN
+  END
+END
+GO
